d59a1e0e09025168c4095dab9aed31198b146dfb
xhwzwka <68984335+xhwzwka@users.noreply.github.com>
2022-02-19 11:00:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function rgbtocmyk(r,g,b) {
-  r = r/2.55;
-  g = g/2.55;
-  b = b/2.55;
-  var k = 100-Math.max(r,g,b);
-  var c = (100-r-k)/(100-k);
-  var m = (100-g-k)/(100-k);
-  var y = (100-b-k)/(100-k);
+  r = r/255;
+  g = g/255;
+  b = b/255;
+  var k = 1-Math.max(r,g,b);
+  var c = (1-r-k)/(1-k)*100;
+  var m = (1-g-k)/(1-k)*100;
+  var y = (1-b-k)/(1-k)*100;
+  
   return [c,m,y,k];
 }
