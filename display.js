f46a46b0870980f1f3fb7908cc2adae7bc1a7974
xhwzwka <68984335+xhwzwka@users.noreply.github.com>
2022-03-06 11:22:03 +0100

    Update and rename main.js to display.js

--- display.js
+++ display.js
@@ -68,29 +68,4 @@ function rcparse() {
   document.getElementById("output2").innerHTML = "Output is given as integers from 0 to 100.";
 }
 
-function rgbtocmyk(r,g,b) {
-
-r = r/255;
-g = g/255;
-b = b/255;
-
-var k = 1-Math.max(r,g,b);
-
-if (k==1) {
-var c = 0;
-var m = 0;
-var y = 0;
-} else {
-var c = (1-r-k)/(1-k);
-var m = (1-g-k)/(1-k);
-var y = (1-b-k)/(1-k);
-}
-
-c = Math.max(0,Math.floor(c*100));
-m = Math.max(0,Math.floor(m*100));
-y = Math.max(0,Math.floor(y*100));
-k = Math.max(0,Math.floor(k*100));
-return [c,m,y,k];
-}
-
 setInterval(checkmodels);
