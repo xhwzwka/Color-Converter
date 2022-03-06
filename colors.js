b94efb58b587d264c6e4c7a72a0af335d14dedc2
xhwzwka <68984335+xhwzwka@users.noreply.github.com>
2022-03-06 11:32:13 +0100

    Update colors.js

--- colors.js
+++ colors.js
@@ -22,3 +22,20 @@ y = Math.max(0,Math.floor(y*100));
 k = Math.max(0,Math.floor(k*100));
 return [c,m,y,k];
 }
+
+function cmyktorgb(c,m,y,k) {
+
+c = c/100;
+m = m/100;
+y = y/100;
+k = k/100;
+
+var r = 255*(1-c)*(1-k);
+var g = 255*(1-m)*(1-k);
+var b = 255*(1-y)*(1-k);
+
+r = Math.max(0,Math.floor(r));
+g = Math.max(0,Math.floor(g));
+b = Math.max(0,Math.floor(b));
+return [r,g,b];
+}
